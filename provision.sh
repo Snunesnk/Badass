f294a703fb77a964f396c1bc0ca552a45243e73a
Simon NUNES <nunesimon85@gmail.com>
2024-02-02 02:55:44 +0100

    Fixed docker installation

--- provision.sh
+++ provision.sh
@@ -8,8 +8,6 @@ sudo apt-get install -y ca-certificates curl
 sudo install -m 0755 -d /etc/apt/keyrings
 sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
-sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-
 
 # Add the repository to Apt sources:
 echo \
@@ -17,6 +15,8 @@ echo \
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update -y
+
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 #Install GNS3
 sudo apt update -y
